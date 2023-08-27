ca2fe07758a3979925c128c9860c51033d0cd37d
Varun <Varun.Ramachandran@techademy.net>
2023-08-27 18:33:16 +0530

    new changes

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Banner.css';
-import cbanner from "../../images/coding-banner-img.jpg"
+import cbanner from "../../images/picofme (2).png"
 
 export default function Banner() {
   return (
     <div className='banner-section container'>
       <div className="banner-content">
-        <h3>UI Developer</h3>
-        <h1>Hi,I'm <span>Varun</span> </h1>
+      <h1>Hi,I'm <span>Varun</span> </h1>
         <h1>From Bangalore ,India</h1>
+        <h3>I am a Front End Web Developer</h3>
+        <p>I have 1 year experinece in ForntEnd web Development.I love to develop web applications using technologies like React,Bootstrap and Tailwind CSS.</p>
+        
       </div>
       <div className='banner-img'>
           <img src={cbanner} alt="banner"/>
